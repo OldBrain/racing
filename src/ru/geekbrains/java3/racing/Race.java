5ab035bffaecaaea73d4c7da15d91decc2678ae9
OldBrain <hell_brain@mail.ru>
2021-02-11 22:52:33 +0400

    Initial commit

---
+++ src/ru/geekbrains/java3/racing/Race.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.java3.racing;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+  private ArrayList<Stage> stages;
+  public ArrayList<Stage> getStages() { return stages; }
+  public Race(Stage... stages) {
+    this.stages = new ArrayList<>(Arrays.asList(stages));
+  }
+}
